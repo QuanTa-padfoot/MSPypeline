89a8b5877ecc8890cb1b35e5a28099cd452bd9b2
QuanTa-padfoot <123317446+QuanTa-padfoot@users.noreply.github.com>
2023-03-17 13:38:42 +0100

    Changed rpy2 version to 3.4.5
    Version 3.4.5 allows for making volcano plots multiple times in a GUI session

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "numpy>=1.17.4",
         "pandas>=1.0.0",
         "scipy>=1.3.1",
-        "rpy2>=3.5.10",
+        "rpy2==3.4.5",
         "tzlocal>=2.0.0",
         #"ruamel_yaml>=0.15.46",
         "matplotlib>=3.1.1",
