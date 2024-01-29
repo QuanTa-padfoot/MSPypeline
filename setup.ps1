bc98baa4141d7cc05e193ead4ba08ae2361ba23c
QuanTa-padfoot <123317446+QuanTa-padfoot@users.noreply.github.com>
2024-01-29 10:36:40 +0100

    move seaborn installation to setup.ps1
    To avoid timed out error when running setup.py

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,8 @@ conda env create -f .\environment.yml
 
 conda activate mspypeline_dev
 
+pip install seaborn==0.12.0
+
 python .\setup.py install
 
 Write-Output "`nInstallation completed. Please remember to define the R_HOME variable before starting the GUI.`n"
