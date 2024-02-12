366c400870e03879fbd7fe675ab9e59839c119fd
QuanTa-padfoot <123317446+QuanTa-padfoot@users.noreply.github.com>
2024-02-12 15:24:07 +0100

    add function to read peptide data from Spectronaut

--- mspypeline/core/MSPPlots/SpectroPlotter.py
+++ mspypeline/core/MSPPlots/SpectroPlotter.py
@@ -74,4 +74,100 @@ class SpectroPlotter(BasePlotter):
         )
         default_kwargs.update(**kwargs)
         return super().from_file_reader(reader_instance, **kwargs)
-        
+
+    def read_peptide_data(self, dir_peptide_data_folder = None, log2_transformed = False, time_level = None):
+        """Read and preprocess the .csv file containing peptide data from Spectronaut. The data is first log2-transformed by default.
+        Rows with multiple protein names are split to separate rows, one for each protein. The time points are detected from the sample names.
+        By default (i.e., if time_level is None), the second-to-last level in analysis_design is used for the detection.
+
+        Parameters
+        ----------
+        dir_peptide_data_folder
+            directory of the folder containing the peptide data, which must be in .csv or .tsv format. If None, MSPypeline will search in a folder named `peptide_data` in the current directory
+            The folder `peptide_data` should contain only 1 .csv or .tsv file to avoid reading the wrong file
+        log2_transformed
+            Whether the data has been log2-transformed. If False, this function will log2-transform the data
+        time_level
+            Which level in analysis_design the time points or doses were declared. If None, the second-to-last level will be used
+
+        Returns
+        --------
+        peptide_df
+            Dataframe containing the log2 expression value of the peptides.
+        timepoints_dict
+            Dictionary specifying the time points or dose for each sample.
+        time_numerical_dict
+            Dictionary specifying the numerical values of the time points or dose for each sample
+        """
+        import numpy as np
+        import os
+        import pandas as pd
+        import re
+        peptide_dir=[]
+        for file in os.listdir(dir_peptide_data_folder):
+            if file.endswith(".xls"):
+                filename, ext = os.path.splitext(file)
+                new_filename = filename + '.csv'
+                old_filedir = os.path.join(dir_peptide_data_folder, file)
+                new_filedir = os.path.join(dir_peptide_data_folder, new_filename)
+                os.replace(old_filedir, new_filedir)
+                peptide_dir.append(new_filedir)
+            elif file.endswith(".csv") or file.endswith(".tsv"):
+                peptide_dir.append(os.path.join(dir_peptide_data_folder, file))
+        try:
+            peptide_dir = peptide_dir[0]
+        except (IndexError):
+            peptide_dir = []
+        for cur_separator in ['\t', ';', ',']:
+            try:
+                peptide_df = pd.read_csv(peptide_dir, delimiter=cur_separator, dtype=str)
+                if peptide_df.shape[1] > 4:
+                    break
+            except (ValueError):
+                pass
+        # rename columns
+        all_quant_cols = peptide_df.filter(regex="Quantity").columns.to_list()
+        all_prefixes = [s.split(".raw")[0] for s in all_quant_cols]
+        all_prefixes = [s.split(".PG")[0] for s in all_prefixes]
+        all_sample_name = [s.split("] ")[1] for s in all_prefixes]
+        rename_dict = {all_quant_cols[i]: all_sample_name[i] for i in range(len(all_quant_cols))} 
+        peptide_df.rename(columns=rename_dict, inplace= True)
+        # convert quantity columns ("all_sample_name") to float and replace "Filtered" with 0
+        peptide_df[all_sample_name] = peptide_df[all_sample_name].replace(',', '.', regex=True)
+        peptide_df[all_sample_name] = peptide_df[all_sample_name].replace("Filtered", np.nan)
+        peptide_df[all_sample_name] = peptide_df[all_sample_name].astype(float)
+        # log2- trasnform the data if not transformed yet
+        if not log2_transformed:
+            peptide_df[all_sample_name] = peptide_df[all_sample_name].transform(lambda x: np.log2(x))
+        # get the time point or dose at the second-to-last level, if applicable
+        n_level = len(all_sample_name[0].split("_"))
+        if time_level is None and n_level > 1:
+            timepoints = [s.split("_")[n_level-2] for s in all_sample_name]
+        elif n_level == 1:
+            timepoints = all_sample_name
+        else:
+            timepoints = [s.split("_")[time_level] for s in all_sample_name]
+        time_numerical = [re.sub("[^\d\.]", "",t) for t in timepoints]
+        try:
+            time_numerical = [float(x) for x in time_numerical]
+            def get_time_unit(x:str):
+                y = x.replace('.', '')
+                non_digit = re.sub(r'\d', '',y)
+                return(non_digit)
+            time_unit = [get_time_unit(x) for x in timepoints]
+            unique_time_unit = list(set(time_unit))
+            # If both min and h are present, convert the min to h in time_numerical
+            if unique_time_unit == ["min", "h"] or unique_time_unit == ["h", "min"]:
+                time_numerical[time_unit == "min"] = time_numerical[time_unit == "min"]/60
+                timepoints_dict = {all_sample_name[i]: timepoints[i] for i in range(len(all_sample_name))}
+                time_numerical_dict = {all_sample_name[i]: timepoints[i] for i in range(len(all_sample_name))}
+            # check if more than 1 unit is present, if so the timepoints and time_numerical are set to empty 
+            elif len(unique_time_unit) > 1:
+                timepoints_dict, time_numerical_dict = {}, {}
+                print("Warning: cannot identify the unit for time points/ dose. Some plotting options for peptide reports will not be available.")
+            else:
+                timepoints_dict = {all_sample_name[i]: timepoints[i] for i in range(len(all_sample_name))}
+                time_numerical_dict = {all_sample_name[i]: timepoints[i] for i in range(len(all_sample_name))}
+        except (ValueError):
+            timepoints_dict, time_numerical_dict = {}, {}
+        return {"peptide_df": peptide_df,"timepoints_dict": timepoints_dict,"time_numerical_dict": time_numerical_dict}
